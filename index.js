18d70ce0abdb3fb7a9296cab68329ebf4646e287
Orta Therox <orta.therox@gmail.com>
2019-09-30 11:10:28 -0400

    Add a workflow file for this repo

--- index.js
+++ index.js
@@ -5,37 +5,40 @@ const core = require('@actions/core');
 const Codeowners = require('codeowners');
 
 async function run() {
-  
-  /** @type {import("@octokit/webhooks").WebhookPayloadPullRequestReview} */
-  // @ts-ignore
-  const payload = github.context.payload
-  if (!validate(payload)) {
-    return
-  }
-  
   const token = core.getInput('GITHUB_TOKEN');
   const octokit = new github.GitHub(token);
+  // const repoDeets = { owner: github.context.repo.owner, repo: github.context.repo.repo }
+  const repoDeets = { owner: "facebook", repo: "jest" }
 
-  const pr = payload.pull_request;
-  const repo = pr.base.repo
-  const review = payload.review
+  const query = searchQuery(`${repoDeets.owner}/${repoDeets.repo}`)
+  const searchResponse = await octokit.graphql(query, {})
 
-  const repoDeets = { owner: repo.owner.login, repo: repo.name }
-  const fileStrings = await getPRChangedFiles(octokit, repoDeets)
-  
-  const filesWhichArentOwned = getFilesNotOwnedByCodeOwner(review.user.login, fileStrings)
+  for (const edge of searchResponse.search.edges) {
+    const pr = edge.node
+    core.info(`\n\nLooking at PR: ${pr.title}`)
 
-  if (filesWhichArentOwned.length > 0) {
-    core.info("Bailing because not all files were covered by the codeowners for this review")
-    core.info(`Missing: ${filesWhichArentOwned.join(", ")}`)
-    return
-  }
-  
-  await commentAndMerge(octokit, repoDeets, pr, review);
+    // PR = { title: string, number: number, reviews: { nodes: { author: {login: string }[] } }
+    console.log(pr)
+
+    const fileStrings = await getPRChangedFiles(octokit, repoDeets, pr.number)
+    
+    const reviewers = pr.reviews.nodes.map(r => r.node.author.login)
+    for (const reviewer of reviewers) {
+
+      const filesWhichArentOwned = getFilesNotOwnedByCodeOwner(reviewer, fileStrings)
+
+      if (filesWhichArentOwned.length > 0) {
+        core.info("Bailing because not all files were covered by the codeowners for this review")
+        core.info(`Missing: ${filesWhichArentOwned.join(", ")}`)
+      } else {
+        await commentAndMerge(octokit, repoDeets, pr, reviewer);
+      }
+    }
+  };
 }
 
-async function commentAndMerge (octokit, repoDeets, pr, review) {
-  await octokit.issues.createComment({ ...repoDeets, issue_number: pr.number, body: `Merging because @${review.user.login} is a code-owner of all the changes - thanks!` });
+async function commentAndMerge (octokit, repoDeets, pr, reviewer) {
+  await octokit.issues.createComment({ ...repoDeets, issue_number: pr.number, body: `Merging because @${reviewer} is a code-owner of all the changes - thanks!` });
   await octokit.pulls.merge({ ...repoDeets, pull_number: pr.number });
 }
 
@@ -45,7 +48,6 @@ function getFilesNotOwnedByCodeOwner(owner, files, cwd) {
   
   for (const file of files) {
     let owners = codeowners.getOwner(file);
-    console.log(owners)
     if (!owners.includes(owner)) {
       filesWhichArentOwned.push(file)
     }
@@ -54,9 +56,31 @@ function getFilesNotOwnedByCodeOwner(owner, files, cwd) {
   return filesWhichArentOwned
 }
 
-async function getPRChangedFiles(octokit, repoDeets) {
+// If you have more than 100 approved + open PRs, you're welcome to make this paginate
+const searchQuery = (repo) => `{
+  search(first: 100, query: "repo:${repo}  is:pr is:open review:approved", type: ISSUE) {
+    edges {
+      node {
+        ... on PullRequest {
+          title
+          number
+          reviews(first: 10, states: APPROVED) {
+            nodes {
+              author {
+                login
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`
+
+
+async function getPRChangedFiles(octokit, repoDeets, prNumber) {
   // https://developer.github.com/v3/pulls/#list-pull-requests-files
-  const options = octokit.pulls.listFiles.endpoint.merge(repoDeets);
+  const options = octokit.pulls.listFiles.endpoint.merge({...repoDeets, pull_number: prNumber });
   
   /** @type { import("@octokit/rest").PullsListFilesResponseItem[]} */
   const files = await octokit.paginate(options)
