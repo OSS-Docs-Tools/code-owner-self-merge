188e39dc85c3403aa964850e3ebbaca91605eda5
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-08-22 13:44:05 +0200

    Trim whitespaces

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ async function run() {
   // Merge if they say they have access
   if (context.eventName === "issue_comment" || context.eventName === "pull_request_review") {
     mergeIfLGTMAndHasAccess()
-  } 
+  }
 }
 
 async function commentOnMergablePRs() {
   if (context.eventName !== "pull_request_target") {
     throw new Error("This function can only run when the workflow specifies `pull_request_target` in the `on:`.")
   }
-  
+
   // Setup
   const cwd = core.getInput('cwd') || process.cwd()
   const octokit = getOctokit(process.env.GITHUB_TOKEN)
@@ -32,7 +32,7 @@ async function commentOnMergablePRs() {
   const thisRepo = { owner: context.repo.owner, repo: context.repo.repo }
 
   core.info(`\nLooking at PR: '${pr.title}' to see if the changed files all fit inside one set of code-owners to make a comment`)
-  
+
   const co = new Codeowners(cwd);
   core.info(`Code-owners file found at: ${co.codeownersFilePath}`)
 
@@ -58,7 +58,7 @@ async function commentOnMergablePRs() {
   if (!ownersWhoHaveAccessToAllFilesInPR.length) {
     console.log("This PR does not have any code-owners who own all of the files in the PR")
     listFilesWithOwners(changedFiles, cwd)
-    
+
     const labelToAdd = core.getInput('if_no_maintainers_add_label')
     if (labelToAdd) {
       const labelConfig = { name: labelToAdd, color: Math.random().toString(16).slice(2, 8) }
@@ -83,7 +83,7 @@ async function commentOnMergablePRs() {
   }
 
   const owners = new Intl.ListFormat().format(ownersWhoHaveAccessToAllFilesInPR);
-  const message = `Thanks for the PR! 
+  const message = `Thanks for the PR!
 
 This section of the codebase is owned by ${owners} - if they write a comment saying "LGTM" then it will be merged.
 ${ourSignature}`
@@ -99,7 +99,7 @@ ${ourSignature}`
 
 async function mergeIfLGTMAndHasAccess() {
   const body = context.payload.comment ? context.payload.comment.body : context.payload.review.body
-  if (!body) { 
+  if (!body) {
     // For debugging #8
     console.log(JSON.stringify(context))
   }
@@ -108,7 +108,7 @@ async function mergeIfLGTMAndHasAccess() {
     console.log("Comment does not include LGTM ('looks good to me') so not merging")
     process.exit(0)
   }
-  
+
   // Setup
   const cwd = core.getInput('cwd') || process.cwd()
   const octokit = getOctokit(process.env.GITHUB_TOKEN)
@@ -117,7 +117,7 @@ async function mergeIfLGTMAndHasAccess() {
   const sender = context.payload.sender.login
 
   core.info(`\n\nLooking at the ${context.eventName} from ${sender} in '${issue.title}' to see if we can merge`)
-  
+
   const changedFiles = await getPRChangedFiles(octokit, thisRepo, issue.number)
   core.info(`Changed files: \n - ${changedFiles.join("\n - ")}`)
 
@@ -172,15 +172,15 @@ async function mergeIfLGTMAndHasAccess() {
 }
 
 /**
- * 
- * @param {string} owner 
- * @param {string[]} files 
- * @param {string} cwd 
+ *
+ * @param {string} owner
+ * @param {string[]} files
+ * @param {string} cwd
  */
 function getFilesNotOwnedByCodeOwner(owner, files, cwd) {
   const filesWhichArentOwned = []
   const codeowners = new Codeowners(cwd);
-  
+
   for (const file of files) {
     const relative = file.startsWith("/") ? file.slice(1) : file
     let owners = codeowners.getOwner(relative);
@@ -193,9 +193,9 @@ function getFilesNotOwnedByCodeOwner(owner, files, cwd) {
 }
 
 /**
- * 
- * @param {string[]} files 
- * @param {string} cwd 
+ *
+ * @param {string[]} files
+ * @param {string} cwd
  */
 function listFilesWithOwners(files, cwd) {
   const codeowners = new Codeowners(cwd);
@@ -213,7 +213,7 @@ function findCodeOwnersForChangedFiles(changedFiles, cwd)  {
   const owners = new Set()
   const labels = new Set()
   const codeowners = new Codeowners(cwd);
-  
+
   for (const file of changedFiles) {
     const relative = file.startsWith("/") ? file.slice(1) : file
     const filesOwners = codeowners.getOwner(relative);
@@ -232,7 +232,7 @@ function findCodeOwnersForChangedFiles(changedFiles, cwd)  {
 async function getPRChangedFiles(octokit, repoDeets, prNumber) {
   // https://developer.github.com/v3/pulls/#list-pull-requests-files
   const options = octokit.pulls.listFiles.endpoint.merge({...repoDeets, pull_number: prNumber });
-  
+
   /** @type { import("@octokit/rest").PullsListFilesResponseItem[]} */
   const files = await octokit.paginate(options)
   const fileStrings = files.map(f => `/${f.filename}`)
